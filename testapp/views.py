04cece9c483d71778e61f6396d6bca1b71a1fcfa
alitou <alitoumob@gmail.com>
2017-07-12 00:32:43 +0430

    TEST APP

---
+++ testapp/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+# Create your views here.
+
+# def hello(request):
+# 	text = "<b>Hello</b>"
+# 	return HttpResponse(text)
+
+def hello(request):
+    return render(request, 'blog/hello.html', {})
